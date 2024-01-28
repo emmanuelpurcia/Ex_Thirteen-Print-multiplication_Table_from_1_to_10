9462f6809c3d7ce484f86b0bc2527d074923201e
emmanuelpurcia <emmanpurcia023@gmail.com>
2024-01-29 00:54:00 +0800

    exercise_thirteen

---
+++ exercise_thirteen program.py
@@ -0,0 +1,8 @@
+# Hello! this program print the multiplication table starting from 1 upto 10
+
+# pseudocode
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(i * j, end=" ")
+    print("\t\t")
